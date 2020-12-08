a139af72f56e657bd2a7f7958efd29b4d15f7fd6
Anom Permadi <anom.p3rm4di@gmail.com>
2020-12-08 23:52:03 +0700

    update oop

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from geometry.persegipanjang import PersegiPanjang
+from geometry.segitiga import Segitiga
+
 print('Use Object Oriented Programming')
+p1 = PersegiPanjang(10, 3)
+print(p1.info())
+print(p1.hitung_luas())
+
+s1 = Segitiga(20, 2)
+print(s1.info())
+print(s1.hitung_luas())
